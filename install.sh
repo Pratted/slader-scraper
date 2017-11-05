001dd4ac346561df68cfc24b3beb7ecc354ded43
Eric <eric@debian.pratt>
2017-11-05 13:38:38 -0500

    Revised install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+sudo apt-get update
 sudo apt-get install qt5-default libqt5webkit5-dev build-essential python-lxml python-pip xvfb
 
 pip install beautifulsoup4 requests dryscrape
 
+
 #prepend the users home directory in python script
-sed -i "1s;^;SLADER_DIR=\"$HOME\"\n;" slader.py
+sed -i "s;^SLADER_DIR.*;SLADER_DIR\=\"$HOME/.slader/\";" slader.py
+
